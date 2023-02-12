8a2fa7bbe12dae4139a393c82468a01edd73d9c8
root <root@ip-172-31-14-6.ap-south-1.compute.internal>
2023-02-12 13:47:10 +0000

    Added network details

--- networks.tf
+++ networks.tf
@@ -1,3 +1,4 @@
+#Creating the vpc
 resource "aws_vpc" "tf-vpc" {
   cidr_block = "172.21.0.0/16"
   tags {
@@ -5,25 +6,28 @@ resource "aws_vpc" "tf-vpc" {
   }
 }
 
+#Creating Subnet-1
 resource "aws_subnet" "tf-web" {
   vpc_id = "${aws_vpc.tf-vpc.id}"
   cidr_block = "172.21.0.0/24"
-  availability_zone = "eu-west-1a"
+  availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
   tags {
   Name = "tf-web"
   }
 }
 
+#Creating Subnet-2
 resource "aws_subnet" "tf-db" {
   vpc_id = "${aws_vpc.tf-vpc.id}"
   cidr_block = "172.21.1.0/24"
-  availability_zone = "eu-west-1b"
+  availability_zone = "ap-south-1b"
   tags {
   Name = "tf-db"
   }
 }
 
+#Creating Internet gateway
 resource "aws_internet_gateway" "igw_tf" {
   vpc_id = "${aws_vpc.tf-vpc.id}"
 
@@ -32,6 +36,7 @@ resource "aws_internet_gateway" "igw_tf" {
   }
 }
 
+#Creating Route table
 resource "aws_route" "rt_tf_web" {
   route_table_id         = "${aws_vpc.tf-vpc.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
