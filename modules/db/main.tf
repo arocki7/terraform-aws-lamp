d084a3db90401182c3d705578ad5ac7cfb80957a
ASHUTOSH PATI <104639044+Ashutosh9909@users.noreply.github.com>
2023-02-14 12:48:40 +0530

    Update db main module

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "test-db" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags {
+  tags = {
     Name = "test-db"
   }
 }
@@ -39,7 +39,7 @@ resource "aws_instance" "db" {
   vpc_security_group_ids = ["${aws_security_group.test-db.id}"]
   subnet_id = "${var.subnet_id}"
 
-  tags {
+  tags = {
     Name = "DB"
     CR = "SHUTDOWN"
   }
